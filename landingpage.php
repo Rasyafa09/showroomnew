5500ebdd30a6fae4804d7aa9537a53a5e8aee8db
AgungRaviano <113073635+AgungRaviano@users.noreply.github.com>
2022-09-30 16:42:14 +0700

    koneksi

---
+++ landingpage.php
@@ -0,0 +1,72 @@
+<?php
+    include "koneksi.php"
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="fcontainer">
+        <div class="fnavbar">
+            <p>Rass Motorcycle</p>
+        <div class="navbar">
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#buy">Buy</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <li><a href="#about">About</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="maintext">
+    <div class="best">
+      <p><h2>BEST SELLER!</h2></p>
+  </div>
+
+  <div class="zx">
+      <p><h3>ZX-25R</h3></p>
+  </div>
+</div>
+  <div class="content">
+    <img src="Gambar/WhatsApp_Image_2022-09-30_at_13.55.22-removebg-preview.png" alt="">
+    <div class="text">
+      <h3 class="spek">SPESIFIKASI</h3>
+      <p><h4>Kawasaki Ninja ZX-25R adalah <br> sebuah sepeda motor sport <br> bermesin 249 cc (15,2 cu in)  dengan  <br> konfigurasi 4 silinder  segaris yang  <br> diproduksi oleh Kawasaki  di <br> Indonesia dan Thailand  sebagai <br> penerus dari Ninja ZX-2R/ZXR250 <br> yang diproduksi antara  tahun 1988 <br> dan 1997</h4></p>
+  </div>
+</div>
+  <div class="button">
+  <div class="but1">
+      <button><a href="#">Buy Now!</a></button>
+  </div>
+  <div class="but2">
+      <button ><a href="#">View More</a></button>
+  </div>
+</div>
+  <img src="/zx25r.png" alt="" class="motor">
+        <div class="image1">
+            <img src="Gambar/WhatsApp Image 2022-09-29 at 19.47.29.jpeg" alt="" width="100%">
+        </div>
+        <h1>Buy NOW!</h1>
+        
+        <div class="card">
+            <img src="Gambar/kawasaki-green-emblem-png-logo-1.png" alt="" style="width:100%" height="50%">
+            <div class="container">
+              <h3>View Sport Page</h3> 
+            </div>
+          </div>
+          <div class="card2">
+            <img src="Gambar/WhatsApp Image 2022-09-29 at 21.12.08.jpeg" alt="" style="width:100%" height="47.5%">
+            <div class="container2">
+              <h3>View Scooter Page</h3> 
+            </div>
+          </div>
+          <div class="about">
+            <h2>About</h2>
+            <p>Website ini bertujuan untuk melihat lihat motor di website ini juga bisa membeli motor di SHOW ROOM ini ada beberapa type motor seperti motor Sport dan motor Scooter di Show room ini juga juga ada beberapa motor yang di unggulkan seperti ZX-25R dan Vespa PRIMA VERA150 yang Spesifikasi nya di atas rata rata melaju dengan kecepatan tinggi di showroom ini juga berpartner dengan VESPA dan NINJA KAWASAKI yang berlokasi diJL. Keadilan Raya No.112, Sukma Jaya, Depok.  -Razz Company</p>
+          </div> 
+</body>
+</html>
